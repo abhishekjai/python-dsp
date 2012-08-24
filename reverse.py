58b6a967d33084635941e1bc50b9fc0d2d02ab3e
Russell Cardullo <russellcardullo@gmail.com>
2012-08-23 23:31:27 -0400

    Switch reverse.py to use AudioFile.

--- reverse.py
+++ reverse.py
@@ -2,9 +2,8 @@
 
 # reverse.py: Reverse input WAV file and save as given output file
  
-import wave
-import struct
 import sys
+from audiofile import *
 
 if len(sys.argv) < 3:
     print "Usage: reverse.py in_file out_file"
@@ -13,30 +12,28 @@ if len(sys.argv) < 3:
 in_filename  = sys.argv[1]
 out_filename = sys.argv[2]
 
-fin = wave.open(in_filename,'r')
-fout = wave.open(out_filename,'w')
-fout.setparams((fin.getnchannels(), 
-                fin.getsampwidth(), 
-                fin.getframerate(), 
-                0,
-                'NONE',
-                'not compressed'))
+fin = AudioFile(in_filename,'r')
+fout = AudioFile(out_filename,'w')
+fout.setparams(fin.getnchannels(), 
+               fin.getsampwidth(), 
+               fin.getframerate(), 
+               0,
+               'NONE',
+               'not compressed')
 
 num_frames = fin.getnframes()
 num_channels = fin.getnchannels()
 
 data = []
 while (fin.tell() < num_frames):
-    frame = fin.readframes(1)
-    data.append(struct.unpack('%dh'%(num_channels), frame))
-
+    frame = fin.read(1)
+    data.append(frame)
 
 data.reverse()
 
 for frame in data:
-    for data in frame:
-        packed_out_data = struct.pack('h', data)
-        fout.writeframes(packed_out_data)
+    for sample in frame:
+        fout.write(sample)
 
 fout.close()  
 fin.close()
